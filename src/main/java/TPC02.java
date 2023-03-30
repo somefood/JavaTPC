b07549b25f50c33023aa966cd2488c54fd4e8ea9
Seokju Hong <iamseokjuhong@gmail.com>
2023-03-31 00:46:19 +0900

    이것만 알자! 프로그래밍의 3대 요소(변수,자료형,할당)

---
+++ src/main/java/TPC02.java
@@ -0,0 +1,25 @@
+package main.java;
+
+public class TPC02 {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 1;
+        b = 1;
+        c = a + b;
+        System.out.println("c = " + c);
+
+        float f;
+        f = 34.5f;
+        System.out.println("f = " + f);
+
+        char d;
+        d = 'A';
+        System.out.println("d = " + d);
+
+        boolean g;
+        g = true;
+
+        System.out.println("g = " + g);
+    }
+}
